af701e7d11dfd4fa1d78ee478723d03985627133
cknoettg <cknoettg@yahoo.com>
2020-05-30 01:26:54 -0400

    Moved async call

---
+++ src/api/fetchShow.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+export const fetchShow = () => {
+    return axios
+      .get(
+        "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+      )
+      .then(response => {return response})
+      .catch(error => console.log("Error fetching show...", error));
+  };
+
+  export default fetchShow;
